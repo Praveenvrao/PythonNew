91b4a93be61eb7e67b94d3b5227dc807d4a57b16
Praveenvrao <vpraveenrao453@gmail.com>
2023-10-10 19:23:46 +0530

    Committing Oct 9th task program

---
+++ Oct9Task.py
@@ -0,0 +1,9 @@
+a = int(input("Choose random number \n"))
+b = int(input("Choose random number \n"))
+c = int(input("Choose random number \n"))
+d = int(input("Choose random number \n"))
+e = int(input("Choose random number \n"))
+f = int(input("Choose random number \n"))
+
+set1 = {a,b,c,d,e,f}
+print(set1)
