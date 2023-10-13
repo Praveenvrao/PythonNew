a96b2cd02914be51063278c8bb315c99d4c496ec
Praveenvrao <vpraveenrao453@gmail.com>
2023-10-13 19:29:12 +0530

    Oct 13 class code

---
+++ Oct13_Operations/Arithematic_oprtns.py
@@ -0,0 +1,18 @@
+#  Airthematic operations
+
+a = int(input("Enter value of a \n"))
+b = int(input("Enter value of b \n"))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a//b)
+print(a**b)
+
+#checking id of variables
+a = 2
+b = 2
+print(id(a))
+print(id(b))
